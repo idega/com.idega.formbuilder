627b49566b098b8661131c07b8048e0ff9895d26
Aleksandras Skrynikovas <alexis@idega.com>
2006-08-21 10:13:43 +0000

    presentation package structure added

---
+++ src/java/com/idega/formbuilder/presentation/FrameComponentTag.java
@@ -0,0 +1,19 @@
+package com.idega.formbuilder.presentation;
+
+import javax.faces.webapp.UIComponentTag;
+
+public class FrameComponentTag extends UIComponentTag {
+
+	@Override
+	public String getComponentType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getRendererType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
