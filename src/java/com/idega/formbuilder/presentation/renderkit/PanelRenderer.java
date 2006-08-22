fc140fcea13509f2a2e9275e30cb8697c1a8bcee
Aleksandras Skrynikovas <alexis@idega.com>
2006-08-22 14:17:08 +0000

    no comment

--- src/java/com/idega/formbuilder/presentation/renderkit/PanelRenderer.java
+++ src/java/com/idega/formbuilder/presentation/renderkit/PanelRenderer.java
@@ -5,143 +5,42 @@ import java.io.IOException;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
+import javax.faces.render.Renderer;
 
-import com.idega.util.RenderUtils;
-import com.idega.webface.WFContainer;
-import com.idega.webface.renderkit.ContainerRenderer;
+import com.idega.formbuilder.presentation.FBPanel;
 
-public class PanelRenderer extends ContainerRenderer {
-	/* (non-Javadoc)
-	 * @see javax.faces.render.Renderer#getRendersChildren()
-	 */
-	public boolean getRendersChildren() {
-		return true;
-	}
+public class PanelRenderer extends Renderer {
 	
-	protected static String HTML_TABLE_TAG="table";
-	protected static String HTML_DIV_TAG = "div";
-
-	public void encodeBegin(FacesContext ctx, UIComponent comp) throws IOException {
-		WFContainer container = (WFContainer) comp;
-		if (!container.isRendered()) {
-			return;
+	public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
+		if ((context == null)|| (component == null)){
+			throw new NullPointerException();
 		}
-		ResponseWriter out = ctx.getResponseWriter();
-		//			out.write("<link type=\"text/css\" href=\"style/webfacestyle.css\"
-		// rel=\"stylesheet\">");
-		//RenderUtils.ensureAllTagsFinished();
+		ResponseWriter writer = context.getResponseWriter();
+		//String clientId = component.getClientId(context);
+		FBPanel panel = (FBPanel) component;
+				
+		writer.startElement("DIV", panel);
+		writer.writeAttribute("style", "border: 5px; border-color: black; width: 200px; height: 120px; background-color: red;","styleClass");
 		
-		//out.startElement(getMarkupElementType(), comp);
-		out.startElement(getMarkupElementType(), null);
+	}
+	
+	public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+		if ((context == null) || (component == null)){
+			throw new NullPointerException();
+		}
+		ResponseWriter writer = context.getResponseWriter();
+		String clientId = component.getClientId(context);
+		FBPanel panel = (FBPanel) component;
 		
-		
-		if (getStyleClass(container) != null) {
-			out.writeAttribute("class", getStyleClass(container), null);
-		}
-		
-		String styleAttribute = getStyleAttributes(container);
-		if (styleAttribute != null) {
-			out.writeAttribute("style", styleAttribute, null);
-		}
-		String title = container.getTitle();
-		if(title!=null){
-			out.writeAttribute("title", title, null);
-			//TODO: investigate if alt should be printed out in a valid HTML document
-			out.writeAttribute("alt", title, null);
-		}
-		
-		//out.endElement(getMarkupElementType());
-	}
-
-	protected String getStyleClass(WFContainer container){
-		if (container.getStyleClass() != null) {
-			return container.getStyleClass();
-		}
-		return null;
+		encodeHeader(panel, writer, clientId);
+		writer.endElement("DIV");
 	}
 	
-	protected String getStyleAttributes(WFContainer container){
-		if (container.getStyleAttribute() != null) {
-			return container.getStyleAttribute();
-		}
-		return null;
-	}
-	
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.faces.render.Renderer#encodeChildren(javax.faces.context.FacesContext,
-	 *      javax.faces.component.UIComponent)
-	 */
-	public void encodeChildren(FacesContext ctx, UIComponent comp) throws IOException {
-		if (!comp.isRendered()) {
-			return;
-		}
-		// TODO Auto-generated method stub
-		super.encodeChildren(ctx, comp);
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.faces.render.Renderer#encodeEnd(javax.faces.context.FacesContext,
-	 *      javax.faces.component.UIComponent)
-	 */
-	public void encodeEnd(FacesContext ctx, UIComponent comp) throws IOException {
-		WFContainer container = (WFContainer) comp;
-		if (!container.isRendered()) {
-			return;
-		}
-		ResponseWriter out = ctx.getResponseWriter();
-		out.endElement(getMarkupElementType());
-	}
-
-	protected String getMarkupElementType() {
-		return HTML_DIV_TAG;
-	}
-	
-	/**
-	 * Renders out the start of a div element with a given styleClass
-	 */
-	protected void renderContainerStart(ResponseWriter out,String styleClass){
-		renderContainerStart(out,getMarkupElementType(),styleClass);
-	}
-	
-	/**
-	 * Renders out end start of a div element
-	 */
-	protected void renderContainerEnd(ResponseWriter out){
-		renderContainerEnd(out,getMarkupElementType());
-	}
-	
-	/**
-	 * Renders out the start of a div element with a given styleClass
-	 */
-	protected void renderContainerStart(ResponseWriter out,String containerType,String styleClass){
-		try {
-			out.startElement(containerType,null);
-			if(styleClass!=null){
-				out.writeAttribute("class",styleClass,null);
-			}
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	protected void renderContainerEnd(ResponseWriter out,String containerType){
-		try {
-			out.endElement(containerType);
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * Render the specified facet.
-	 */
-	protected void renderFacet(FacesContext context,UIComponent parent, String facetName) throws IOException {
-		RenderUtils.renderFacet(context,parent,facetName);
+	private void encodeHeader(UIComponent component, ResponseWriter writer, String clientId) throws IOException {
+		writer.startElement("DIV", component);
+		writer.startElement("P", component);
+		writer.writeText("This is normal", null);
+		writer.endElement("P");
+		writer.endElement("DIV");
 	}
 }
