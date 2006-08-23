011bc2a5f6bb6b5a2149a88f35d0c8fad1dc8e69
Vytautas Civilis <civilis@idega.com>
2006-08-23 09:53:58 +0000

    styleClass property added

--- src/java/com/idega/formbuilder/presentation/FBToolbarTag.java
+++ src/java/com/idega/formbuilder/presentation/FBToolbarTag.java
@@ -4,6 +4,16 @@ import org.apache.myfaces.shared_tomahawk.taglib.UIComponentTagBase;
 
 public class FBToolbarTag extends UIComponentTagBase {
 
+	private String styleClass;
+	
+	public String getStyleClass() {
+		return styleClass;
+	}
+
+	public void setStyleClass(String styleClass) {
+		this.styleClass = styleClass;
+	}
+
 	public FBToolbarTag() {
 		super();
 	}
@@ -21,4 +31,6 @@ public class FBToolbarTag extends UIComponentTagBase {
 	public String getRendererType() {
 		return "fb_toolbar";
 	}
+	
+	
 }
