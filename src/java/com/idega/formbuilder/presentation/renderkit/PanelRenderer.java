d467455ec450503b4d3508415e70f92062bc5699
Aleksandras Skrynikovas <alexis@idega.com>
2006-08-23 15:17:07 +0000

    changed panel renderer

--- src/java/com/idega/formbuilder/presentation/renderkit/PanelRenderer.java
+++ src/java/com/idega/formbuilder/presentation/renderkit/PanelRenderer.java
@@ -12,35 +12,36 @@ import com.idega.formbuilder.presentation.FBPanel;
 public class PanelRenderer extends Renderer {
 	
 	public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
-		if ((context == null)|| (component == null)){
-			throw new NullPointerException();
-		}
-		ResponseWriter writer = context.getResponseWriter();
-		//String clientId = component.getClientId(context);
-		FBPanel panel = (FBPanel) component;
-				
-		writer.startElement("DIV", panel);
-		writer.writeAttribute("style", "border: 5px; border-color: black; width: 200px; height: 120px; background-color: red;","styleClass");
 		
 	}
 	
 	public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
-		if ((context == null) || (component == null)){
-			throw new NullPointerException();
-		}
 		ResponseWriter writer = context.getResponseWriter();
-		String clientId = component.getClientId(context);
 		FBPanel panel = (FBPanel) component;
-		
-		encodeHeader(panel, writer, clientId);
+		writer.startElement("DIV", panel);
+		writer.writeAttribute("style", "position: absolute; top: " + panel.getTop() + "px; left: " + panel.getLeft() + "px;","position");
+		writer.writeAttribute("class", panel.getStyleClass(), "styleClass");
+			writer.startElement("DIV", panel);
+			writer.writeAttribute("id", "fb_panel_header", "headerStyle");
+				writer.startElement("DIV", panel);
+				writer.writeAttribute("id", "fb_panel_header_text", "headerTextStyle");
+				writer.writeText(panel.getTitle(), null);
+				writer.endElement("DIV");
+				
+				writer.startElement("DIV", panel);
+				writer.writeAttribute("id", "fb_panel_header_icon", "headerIconStyle");
+				writer.endElement("DIV");
+			writer.endElement("DIV");
+			writer.startElement("DIV", panel);
+			writer.endElement("DIV");
 		writer.endElement("DIV");
 	}
 	
-	private void encodeHeader(UIComponent component, ResponseWriter writer, String clientId) throws IOException {
-		writer.startElement("DIV", component);
-		writer.startElement("P", component);
-		writer.writeText("This is normal", null);
-		writer.endElement("P");
-		writer.endElement("DIV");
+	public void decode(FacesContext context, UIComponent component) {
+		
+	}
+	
+	public void encodeChildren(FacesContext context, UIComponent component) {
+		
 	}
 }
