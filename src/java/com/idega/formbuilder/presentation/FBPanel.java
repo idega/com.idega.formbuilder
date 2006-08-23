34a9082b00cd441cf451d10656ffd64786734299
Aleksandras Skrynikovas <alexis@idega.com>
2006-08-23 15:18:29 +0000

    FBPanel custom component updated

--- src/java/com/idega/formbuilder/presentation/FBPanel.java
+++ src/java/com/idega/formbuilder/presentation/FBPanel.java
@@ -1,63 +1,64 @@
 package com.idega.formbuilder.presentation;
 
-import java.io.IOException;
-
-import javax.faces.component.UIComponent;
 import javax.faces.component.UIPanel;
-import javax.faces.context.FacesContext;
-import javax.faces.context.ResponseWriter;
+import javax.faces.el.ValueBinding;
 
 public class FBPanel extends UIPanel {
 	
-	private static String RENDERER_TYPE="fb_panel";
+	private static final String RENDERER_TYPE = "fb_panel";
+	private static final String EXPAND_IMG = "";
+	private static final String COLLAPSE_IMG = "";
 	
-	private static final String EXPAND_IMG = "expand.jpg";
-	private static final String COLLAPSE_IMG = "collapse.jpg";
+	private String title;
+	private String styleClass;
+	private String top;
+	private String left;
+	//private boolean expanded;
 	
-	private String styleClass = null;
-	private String title = null;
-	private boolean expanded = true;
 	
+	public String getStyleClass() {
+		return styleClass;
+	}
+
+	public void setStyleClass(String styleClass) {
+		this.styleClass = styleClass;
+	}
+
+	public String getLeft() {
+		return left;
+	}
+
+	public void setLeft(String left) {
+		this.left = left;
+	}
+
+	public String getTop() {
+		return top;
+	}
+
+	public void setTop(String top) {
+		this.top = top;
+	}
+
 	public FBPanel() {
 		super();
-		//this.setRendererType(RENDERER_TYPE);
+		this.setRendererType(RENDERER_TYPE);
 	}
 	
 	public String getFamily() {
 		return ("formbuilder");
 	}
-	
-	public void encodeBegin(FacesContext context) throws IOException {
-		if ((context == null)|| (this == null)){
-			throw new NullPointerException();
+
+	public String getTitle() {
+		if(this.title != null) {
+			return this.title;
 		}
-		System.out.println("CHECKPOINT");
-		ResponseWriter writer = context.getResponseWriter();
-		//String clientId = component.getClientId(context);
-		FBPanel panel = (FBPanel) this;
-				
-		writer.startElement("DIV", panel);
-		writer.writeAttribute("style", "border: 5px; border-color: black; width: 200px; height: 120px; background-color: red;","styleClass");
-		
+		ValueBinding binding = getValueBinding("title");
+		return (binding != null) ? (String) binding.getValue(getFacesContext()) : null;
 	}
-	
-	public void encodeEnd(FacesContext context) throws IOException {
-		if ((context == null) || (this == null)){
-			throw new NullPointerException();
-		}
-		ResponseWriter writer = context.getResponseWriter();
-		String clientId = this.getClientId(context);
-		FBPanel panel = (FBPanel) this;
-		
-		encodeHeader(panel, writer, clientId);
-		writer.endElement("DIV");
+
+	public void setTitle(String title) {
+		this.title = title;
 	}
-	
-	private void encodeHeader(UIComponent component, ResponseWriter writer, String clientId) throws IOException {
-		writer.startElement("DIV", component);
-		writer.startElement("P", component);
-		writer.writeText("This is normal", null);
-		writer.endElement("P");
-		writer.endElement("DIV");
-	}
+
 }
--- src/java/com/idega/formbuilder/presentation/FBPanelTag.java
+++ src/java/com/idega/formbuilder/presentation/FBPanelTag.java
@@ -1,10 +1,44 @@
 package com.idega.formbuilder.presentation;
 
 import javax.faces.component.UIComponent;
-import javax.faces.webapp.UIComponentTag;
+import javax.faces.el.ValueBinding;
 
+import org.apache.myfaces.shared_tomahawk.taglib.UIComponentTagBase;
 
-public class FBPanelTag extends UIComponentTag {
+public class FBPanelTag extends UIComponentTagBase {
+	
+	private String title;
+	private String styleClass;
+	private String top;
+	private String left;
+	
+	public String getLeft() {
+		return left;
+	}
+
+	public void setLeft(String left) {
+		this.left = left;
+	}
+
+	public String getTop() {
+		return top;
+	}
+
+	public void setTop(String top) {
+		this.top = top;
+	}
+
+	public String getStyleClass() {
+		return styleClass;
+	}
+
+	public void setStyleClass(String styleClass) {
+		this.styleClass = styleClass;
+	}
+
+	public FBPanelTag() {
+		super();
+	}
 	
 	public String getRendererType() {
 		return "fb_panel";
@@ -16,9 +50,38 @@ public class FBPanelTag extends UIComponentTag {
 
 	public void release() {
 		super.release();
+		this.title = null;
 	}
 	
 	public void setProperties(UIComponent component) {
-	    super.setProperties(component);  
+	    super.setProperties(component);
+	    if (component != null) {
+	    	FBPanel panel = (FBPanel)component;
+			if(this.title != null) {
+				if (isValueReference(this.title)) {
+	                ValueBinding vb = getFacesContext().getApplication().createValueBinding(this.title);
+	                panel.setValueBinding("title", vb);
+	            } else {
+	            	panel.setTitle(this.title);
+	            }
+			}
+			if(this.styleClass != null) {
+				panel.setStyleClass(this.styleClass);
+			}
+			if(this.top != null) {
+				panel.setTop(this.top);
+			}
+			if(this.left != null) {
+				panel.setLeft(this.left);
+			}
+	    }
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
 	}
 }
