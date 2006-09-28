44cf5ab844692992bcb58db2b6af416c6b245ed7
Aleksandras Skrynikovas <alexis@idega.com>
2006-09-28 12:36:13 +0000

    GenericFormField

---
+++ src/java/com/idega/formbuilder/presentation/renderkit/GenericFormComponentRenderer.java
@@ -0,0 +1,50 @@
+package com.idega.formbuilder.presentation.renderkit;
+
+import java.io.IOException;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import javax.faces.render.Renderer;
+
+import org.w3c.dom.Element;
+
+import com.idega.formbuilder.business.form.IFormBuilder;
+import com.idega.formbuilder.presentation.FBGenericFormComponent;
+
+public class GenericFormComponentRenderer extends Renderer {
+	
+	public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
+		
+	}
+	
+	public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+		ResponseWriter writer = context.getResponseWriter();
+		FBGenericFormComponent field = (FBGenericFormComponent) component;
+		Element element = null;
+		try {
+			IFormBuilder fbInstance = (IFormBuilder) context.getExternalContext().getSessionMap().get("formbuilderInstance");
+			/*
+			if(field == null) {
+				System.out.println("COMPONENT IS NULL");
+			}
+			else if(field.getType() == null) {
+				System.out.println("TYPE IS NULL");
+			}
+			*/
+			element = fbInstance.createFormComponent(field.getType(), null);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		GenericFieldParser.renderNode(element, field, writer);
+	}
+	
+	public void decode(FacesContext context, UIComponent component) {
+		
+	}
+	
+	public void encodeChildren(FacesContext context, UIComponent component) {
+		
+	}
+
+}
--- src/java/com/idega/formbuilder/presentation/renderkit/PanelRenderer.java
+++ src/java/com/idega/formbuilder/presentation/renderkit/PanelRenderer.java
@@ -12,14 +12,9 @@ import com.idega.formbuilder.presentation.FBPanel;
 public class PanelRenderer extends Renderer {
 	
 	public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
-		
-	}
-	
-	public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
 		ResponseWriter writer = context.getResponseWriter();
 		FBPanel panel = (FBPanel) component;
 		writer.startElement("DIV", panel);
-		//writer.writeAttribute("style", "position: absolute; top: " + panel.getTop() + "px; left: " + panel.getLeft() + "px;","position");
 		writer.writeAttribute("class", panel.getStyleClass(), "styleClass");
 			writer.startElement("DIV", panel);
 			writer.writeAttribute("id", "fb_panel_header", "headerStyle");
@@ -30,9 +25,16 @@ public class PanelRenderer extends Renderer {
 				
 				writer.startElement("DIV", panel);
 				writer.writeAttribute("id", "fb_panel_header_icon", "headerIconStyle");
+				writer.startElement("IMG", panel);
+				writer.writeAttribute("src", panel.getCurrentIcon(), "headerIcon");
+				writer.endElement("IMG");
 				writer.endElement("DIV");
 			writer.endElement("DIV");
 			writer.startElement("DIV", panel);
+	}
+	
+	public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+		ResponseWriter writer = context.getResponseWriter();
 			writer.endElement("DIV");
 		writer.endElement("DIV");
 	}
