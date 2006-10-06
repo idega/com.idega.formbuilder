1fe273fad0b9a69e8bfb55a3fbd33481f0dd3730
Aleksandras Skrynikovas <alexis@idega.com>
2006-10-06 08:44:22 +0000

    added support for scriptaculous

--- resources/javascript/formbuilder.js
+++ resources/javascript/formbuilder.js
@@ -3,16 +3,13 @@ function dropHandler(idOfDraggedItem,targetId,x,y) {
 	document.forms['workspaceform1'].elements['selected_field_type'].value=fieldType;
 	document.getElementById('add_field_button').onclick();
 }
-
 function selectFormField(selectedField) {
 	document.forms['workspaceform1'].elements['selected_field_id'].value=selectedField.parentNode.id;
 	document.getElementById('select_field_button').onclick();
 }
-
 function selectFormHeader() {
 	document.getElementById('select_form_header_button').onclick();
 }
-
 function setup(listObjId) {
 	var listObj = document.getElementById(listObjId);
 	var subDivs = listObj.getElementsByTagName('li');
@@ -31,12 +28,29 @@ function displayMessage(url) {
 function closeMessage() {
 	messageObj.close();	
 }
+function empty() {
+	alert('Success');
+}
+function temp(element) {
+	alert(element.id);
+	/*for(i=0;i<element.getElementsByTagName('div').length;i++) {
+		alert(element.childNodes[i].id);
+	}*/
+	actionmanager.rebuildFormComponentsTree(empty);
+}
+
+/*Setup drag and drop from palette to main area with DHTMLGoodies*/
 var dragDropObj = new DHTMLSuite_dragDrop();
 setup('workspaceform1:firstlist');
 /*setup('firstlist');*/
 dragDropObj.init();
+
+/*Setup modal message windows functionality*/
 messageObj = new DHTML_modalMessage();
 messageObj.setShadowOffset(5);
 
+/*Setup form components drag and drop functionality with scriptaculous*/
+Position.includeScrollOffsets = true;
+Sortable.create("dropBox",{dropOnEmpty:true,tag:"div",only:"form_element","onUpdate":temp,containment:["dropBox"],scroll:"dropBox",constraint:false});
 
 								
