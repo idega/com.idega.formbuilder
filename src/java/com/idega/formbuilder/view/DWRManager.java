480473579c2f25fd3d7f7c3066e3166d94f08fc0
Aleksandras Skrynikovas <alexis@idega.com>
2006-10-12 17:12:44 +0000

    DWR manager

--- src/java/com/idega/formbuilder/view/DWRManager.java
+++ src/java/com/idega/formbuilder/view/DWRManager.java
@@ -11,6 +11,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.chiba.xml.dom.DOMUtil;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Text;
 
 import com.idega.formbuilder.FormbuilderViewManager;
 import com.idega.formbuilder.business.form.manager.IFormManager;
@@ -31,15 +32,40 @@ public class DWRManager implements Serializable {
         try {
           DocumentBuilder builder = factory.newDocumentBuilder();
           document = builder.newDocument();
-          root = (Element) document.createElement("DIV");
-          root.setAttribute("class", "form_element");
-          document.appendChild(root);
-          Element text = document.createElement("H1");
-          text.appendChild(document.createTextNode("Some text"));
-          root.appendChild(text);
+          root = (Element) document.createElement("div");
+          root.setAttribute("class", "formElement");
+          root.setAttribute("id", "fbcomp_1");
+          Element label = (Element) document.createElement("label");
+          label.setAttribute("class", "label");
+          label.setAttribute("id", "fbcomp_2-label");
+          label.setAttribute("for", "fbcomp_1-value");
+          Text text = (Text) document.createTextNode("Some label");
+          label.appendChild(text);
+          root.appendChild(label);
+          Element input = (Element) document.createElement("input");
+          input.setAttribute("class", "value");
+          input.setAttribute("id", "fbcomp_2-value");
+          input.setAttribute("name", "d_fbcomp_2");
+          input.setAttribute("type", "text");
+          input.setAttribute("value", "Some text");
+          Element para = (Element) document.createElement("p");
+          Text text2 = (Text) document.createTextNode("Some rtext ok okoko");
+          para.appendChild(text2);
+          //input.appendChild(para);
+          Element input2 = (Element) document.createElement("input");
+          input2.setAttribute("class", "value");
+          input2.setAttribute("id", "fbcomp_2-value2");
+          input2.setAttribute("name", "d_fbcomp_22");
+          input2.setAttribute("type", "text");
+          input2.setAttribute("value", "Some text");
+          
+          root.appendChild(input);
+          root.appendChild(para);
+          root.appendChild(input2);
         } catch (ParserConfigurationException pce) {
             pce.printStackTrace();
         }
+        System.out.println("Hand composed element DOM");
         DOMUtil.prettyPrintDOM(root);
 		
 		//System.out.println("INSIDE METHOD");
@@ -47,9 +73,7 @@ public class DWRManager implements Serializable {
 		System.out.println("Value (type) is: " + type);
 		String elementId = formManagerInstance.createFormComponent(type, null);
 		Element element = formManagerInstance.getLocalizedFormHtmlComponent(elementId, new Locale("en"));
-		element.setAttribute("class", "formElement");
-		element.setAttribute("onClick", "tempCheck()");
-		element.setAttribute("style", "background: Red;");
+		System.out.println("Generated element DOM");
 		DOMUtil.prettyPrintDOM(element);
 		System.out.println(element);
 		//root.appendChild(element.getFirstChild().cloneNode(true));
