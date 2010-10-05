dac794b9f2008f4be4c67fb5136c56adcd668c49
Valdas Žemaitis <valdas@idega.com>
2010-10-05 15:21:39 +0300

    Using logger instead of system.out...

--- src/java/com/idega/formbuilder/presentation/beans/FormSelectComponent.java
+++ src/java/com/idega/formbuilder/presentation/beans/FormSelectComponent.java
@@ -2,6 +2,7 @@ package com.idega.formbuilder.presentation.beans;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import com.idega.formbuilder.util.FBUtil;
 import com.idega.xformsmanager.business.component.ComponentSelect;
@@ -13,6 +14,7 @@ public class FormSelectComponent extends FormComponent {
 		this.component = component;
 	}
 
+	@Override
 	public ComponentSelect getComponent() {
 		return (ComponentSelect) component;
 	}
@@ -21,26 +23,31 @@ public class FormSelectComponent extends FormComponent {
 		this.component = component;
 	}
 	
+	@Override
 	public void setExternalSrc(String externalSrc) {
 		getComponent().getProperties().setExternalDataSrc(externalSrc);
 	}
 	
+	@Override
 	public String getExternalSrc() {
 		return getComponent().getProperties().getExternalDataSrc();
 	}
 	
+	@Override
 	public List<ItemBean> getItems() {
 		if(getComponent() != null && getComponent().getProperties() != null && getComponent().getProperties().getItemset() != null) {
-			System.out.println("items="+getComponent().getProperties().getItemset());
+			Logger.getLogger(getClass().getName()).info("items="+getComponent().getProperties().getItemset());
 			return getComponent().getProperties().getItemset().getItems(FBUtil.getUILocale());
 		}
 		return new ArrayList<ItemBean>();
 	}
 	
+	@Override
 	public void setItems(List<ItemBean> items) {
 		getComponent().getProperties().getItemset().setItems(FBUtil.getUILocale(), items);
 	}
 	
+	@Override
 	public String getDataSrc() {
 		if(getComponent().getProperties().getDataSrcUsed() != null) {
 			return getComponent().getProperties().getDataSrcUsed().toString();
@@ -49,6 +56,7 @@ public class FormSelectComponent extends FormComponent {
 		}
 	}
 
+	@Override
 	public void setDataSrc(String dataSrc) {
 		getComponent().getProperties().setDataSrcUsed(Integer.parseInt(dataSrc));
 	}
