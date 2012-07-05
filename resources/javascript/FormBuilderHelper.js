051f7b4eb0a2f11b0f21e2d293327a167315bbfd
Valdas Žemaitis <valdas@idega.com>
2012-07-05 10:35:28 +0300

    Changed the way verify if checkbox is checked

--- resources/javascript/FormBuilderHelper.js
+++ resources/javascript/FormBuilderHelper.js
@@ -1796,7 +1796,7 @@ function getSaveScourceFormButton() {
 }
 
 FormBuilder.toggleUseHtmlEditor = function(checkBoxId, componentId, type, event) {
-	var value = jQuery('#' + checkBoxId).attr('checked');
+	var value = jQuery('#' + checkBoxId).attr('checked') == 'checked';
 	//saveComponentProperty(componentId, type, value, event);
 	PropertyManager.saveComponentProperty(componentId, type, value, currentCallback);
 }
