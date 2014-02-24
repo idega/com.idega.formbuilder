290b21e2ed815f813ce1ac6da0d0421356664c61
Valdas Žemaitis <valdas@idega.com>
2014-02-24 09:30:59 +0200

    Removed warning

--- src/java/com/idega/formbuilder/presentation/beans/DataSourceList.java
+++ src/java/com/idega/formbuilder/presentation/beans/DataSourceList.java
@@ -10,7 +10,6 @@ import java.util.logging.Logger;
 
 import javax.faces.model.SelectItem;
 
-import com.idega.xformsmanager.business.component.properties.PropertiesSelect;
 import com.idega.formbuilder.IWBundleStarter;
 import com.idega.idegaweb.BundleLocalizationMap;
 import com.idega.presentation.ui.SelectOption;
@@ -18,16 +17,17 @@ import com.idega.util.config.Config;
 import com.idega.util.config.ConfigException;
 import com.idega.util.config.ConfigFactory;
 import com.idega.webface.WFUtil;
+import com.idega.xformsmanager.business.component.properties.PropertiesSelect;
 
 public class DataSourceList implements Serializable {
-	
+
 	private static final long serialVersionUID = -1462694112346788168L;
 	private static final String select_sources = "select-external_data_sources";
 	private static final Logger logger = Logger.getLogger(DataSourceList.class.getName());
-	
+
 	public static String localDataSrc = new Integer(PropertiesSelect.LOCAL_DATA_SRC).toString();
 	public static String externalDataSrc = new Integer(PropertiesSelect.EXTERNAL_DATA_SRC).toString();
-	
+
 	private List<SelectItem> sources = new ArrayList<SelectItem>();
 	private List<SelectOption> extDataSources;
 
@@ -44,33 +44,32 @@ public class DataSourceList implements Serializable {
 	public void setSources(List<SelectItem> sources) {
 		this.sources = sources;
 	}
-	
-	@SuppressWarnings("unchecked")
+
 	public List<SelectOption> getExternalDataSources() {
-	
+
 		if(extDataSources == null) {
-			
+
 			extDataSources = new ArrayList<SelectOption>();
 			extDataSources.add(new SelectOption("Choose", ""));
-			
+
 			try {
-				Map<String, String> localizedStrings = (BundleLocalizationMap) ((HashMap) WFUtil.getBeanInstance("localizedStrings")).get(IWBundleStarter.IW_BUNDLE_IDENTIFIER);
-				
+				Map<String, String> localizedStrings = (BundleLocalizationMap) ((HashMap<?, ?>) WFUtil.getBeanInstance("localizedStrings")).get(IWBundleStarter.IW_BUNDLE_IDENTIFIER);
+
 				Config cfg = ConfigFactory.getInstance().getConfig(IWBundleStarter.IW_BUNDLE_IDENTIFIER, IWBundleStarter.FB_CFG_FILE);
 				Map<String, String > properties = cfg.getProperies(select_sources);
-				
+
 				if(properties != null) {
-					
+
 					for (String srcKey : properties.keySet())
 						extDataSources.add(new SelectOption(localizedStrings.get(srcKey+".label"), properties.get(srcKey)));
 				}
-					
-				
+
+
 			} catch (ConfigException e) {
 				logger.log(Level.SEVERE, "Unable to load select external data sources list. Config key provided: "+select_sources, e);
 			}
 		}
-		
+
 		return extDataSources;
 	}
 }
